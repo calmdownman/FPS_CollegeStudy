684a762349adae990abbbc8f2b269a1a7585625f
calmdownman <cskagud7@naver.com>
2023-03-15 19:36:59 +0900

    week 1 - class 1 15:37

---
+++ Assets/Scripts/CamRotate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouse_X = Input.GetAxis("Mouse X");
+        float mouse_Y = Input.GetAxis("Mouse Y");
+
+    }
+}
